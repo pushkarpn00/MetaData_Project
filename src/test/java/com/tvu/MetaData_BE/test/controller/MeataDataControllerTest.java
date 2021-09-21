aa71e914c25725dfc824141123958d8bfb82d3c3
pushkarpn00 <pushkarpn00@gmail.com>
2021-09-21 21:06:55 +0530

    Commited for test cases

---
+++ src/test/java/com/tvu/MetaData_BE/test/controller/MeataDataControllerTest.java
@@ -0,0 +1,13 @@
+package com.tvu.MetaData_BE.test.controller;
+
+import org.junit.Test;
+import com.tvu.Metadata_BE.ApplicationLuncher;
+
+public class MeataDataControllerTest {
+
+	@Test
+	public void main() {
+
+		ApplicationLuncher.main(new String[] {});
+	}
+}
---
+++ src/test/java/com/tvu/MetaData_BE/test/stub/ControllerTest.java
@@ -0,0 +1,47 @@
+package com.tvu.MetaData_BE.test.stub;
+/*package com.tvu.MetaData_Project;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import com.google.common.net.MediaType;
+
+public class ControllerTest extends AbstractTest {
+
+	@Override
+	   @Before
+	   public void setUp() {
+	      super.setUp();
+	   }
+	   @Test
+	   public void getProductsList() throws Exception {
+	      String uri = "/products";
+	      MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)
+	         .accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
+	      
+	      int status = mvcResult.getResponse().getStatus();
+	      assertEquals(200, status);
+	      String content = mvcResult.getResponse().getContentAsString();
+	      Product[] productlist = super.mapFromJson(content, Product[].class);
+	      assertTrue(productlist.length > 0);
+	   }
+	   @Test
+	   public void createProduct() throws Exception {
+	      String uri = "/products";
+	      Product product = new Product();
+	      product.setId("3");
+	      product.setName("Ginger");
+	      String inputJson = super.mapToJson(product);
+	      MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.post(uri)
+	         .contentType(MediaType.APPLICATION_JSON_VALUE)
+	         .content(inputJson)).andReturn();
+	      
+	      int status = mvcResult.getResponse().getStatus();
+	      assertEquals(201, status);
+	      String content = mvcResult.getResponse().getContentAsString();
+	      assertEquals(content, "Product is created successfully");
+	   }
+}
+*/
